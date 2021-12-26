15c4559924de1af3429937960a94d27beb3e64a9
jameswesc <j.gregory@smashdelta.com>
2021-12-27 10:31:19 +1100

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ export function App() {
     const r2 = useState(() => fxrand())
     const r3 = useState(() => fxrand())
 
-    console.log('HELLO WORLD')
+    console.log({ r1, r2, r3 })
 
     return (
         <h1 style={{ backgroundColor: '#ffffff', color: '#000000' }}>
-            {fxhash}
+            {fxhash} <br />
+            {r1} <br />
+            {r2} <br />
+            {r3}
         </h1>
     )
 }
